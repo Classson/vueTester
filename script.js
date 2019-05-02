869591f81294f06f78298eac04a6474ee9b457c5
Alex <Jillbk22@gmail.com>
2019-05-02 17:22:04 -0400

    added conditional image rendering

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 let app = new Vue({
   el: '#app',
   data: {
-    product: "Puppies"
+    product: "Puppies",
+    description: "yup, puppies sure are cool. Don't put them in pants though. They don't like it.",
+    horImage: "https://odditymall.com/includes/content/bottom-half-dog-pants-thumb.jpg",
+    vertImage: "https://mozziepants.com/wp-content/uploads/2015/11/2015-11-19-07.52.43-2.jpg",
+    altText: "dog in pants",
+    link: "https://www.today.com/pets/how-would-dog-wear-pants-internet-finally-has-answer-t64486",
+    horizontal: false
   }
 })
