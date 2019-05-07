33b478bd03a73024aa265069dde60eb7952de2c7
Alex <Jillbk22@gmail.com>
2019-05-07 15:55:40 -0400

    added event handling and style binding

--- script.js
+++ script.js
@@ -2,13 +2,37 @@
 let app = new Vue({
   el: '#app',
   data: {
-    product: "Puppies",
-    description: "yup, puppies sure are cool. Don't put them in pants though. They don't like it.",
-    horImage: "https://odditymall.com/includes/content/bottom-half-dog-pants-thumb.jpg",
-    vertImage: "https://mozziepants.com/wp-content/uploads/2015/11/2015-11-19-07.52.43-2.jpg",
+    product: 'Puppies',
+    type: 'Wonderful',
+    description: "yup, puppies sure are cool but don't you ever wonder how they would wear pants?",
+    image: "https://odditymall.com/includes/content/bottom-half-dog-pants-thumb.jpg",
     altText: "dog in pants",
+    selectedColor: 'yellow',
     link: "https://www.today.com/pets/how-would-dog-wear-pants-internet-finally-has-answer-t64486",
-    horizontal: false,
-    coolStuff: ["furry", "friendly", "can learn tricks", "gets a lot of instagram likes"]
+    coolStuff: ["furry", "friendly", "can learn tricks", "gets a lot of instagram likes"],
+    vertView: {view: 'vertical', image: 'https://mozziepants.com/wp-content/uploads/2015/11/2015-11-19-07.52.43-2.jpg', color: 'grey', likes: 0},
+    horzView: {view: 'horizontal', image: "https://odditymall.com/includes/content/bottom-half-dog-pants-thumb.jpg", color: 'yellow', likes: 0}
+  },
+
+  methods: {
+    addVert(){
+      this.vertView.likes++
+    },
+
+    addHorz(){
+      this.horzView.likes++
+    },
+
+    toggleImage(view){
+      this.image = view.image
+      this.selectedColor = view.color
+    }
+  },
+
+  computed: {
+    title(){
+      return `${this.type} ${this.product}`
+    }
   }
+
 })
